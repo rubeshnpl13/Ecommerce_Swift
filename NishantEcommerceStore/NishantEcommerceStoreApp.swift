6666061665ea5df1d15ac64491fb02b63ee6fd1e
manujakoirala1 <rubeshnepal@gmail.com>
2023-01-13 19:25:53 +0545

    Initial Commit

---
+++ NishantEcommerceStore/NishantEcommerceStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  NishantEcommerceStoreApp.swift
+//  NishantEcommerceStore
+//
+//  Created by Nishanta Nepal on 13/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NishantEcommerceStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
